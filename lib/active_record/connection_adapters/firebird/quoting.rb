eea4c175af5e6a0a65b5ec2dbed7e859c34fadf8
Emilio Blanco <emilioeduardob@gmail.com>
2025-04-03 09:57:10 -0300

    fix quote

--- lib/active_record/connection_adapters/firebird/quoting.rb
+++ lib/active_record/connection_adapters/firebird/quoting.rb
@@ -2,4 +2,14 @@ module ActiveRecord::ConnectionAdapters::Firebird::Quoting
   def quoted_date(value)
     super.sub(/(\.\d{6})\z/, $1.to_s.first(5))
   end
+
+  module ClassMethods
+    def quote_column_name(name)
+      %Q("#{name}")
+    end
+  end
+
+  def self.included(base)
+    base.extend(ClassMethods)
+  end
 end
