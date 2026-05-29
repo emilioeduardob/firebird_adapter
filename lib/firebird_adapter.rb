de834b7d7a5a2becc8b738cf0a24240e39fb9076
Emilio Blanco <emilioeduardob@gmail.com>
2026-05-29 07:39:29 -0300

    Fix missing ActiveSupport require in entry point
    lib/firebird_adapter.rb uses ActiveSupport.on_load but did not require
    active_support first. In Rails 8.1 this raises NameError during boot.

--- lib/firebird_adapter.rb
+++ lib/firebird_adapter.rb
@@ -1,3 +1,5 @@
+require 'active_support'
+
 # Require the adapter
 ActiveSupport.on_load :active_record do
   ActiveRecord::ConnectionAdapters.register("firebird", "ActiveRecord::ConnectionAdapters::FirebirdAdapter")
